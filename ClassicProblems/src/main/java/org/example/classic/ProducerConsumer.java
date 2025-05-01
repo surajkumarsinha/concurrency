9cc9f4eeeedfe1f4631ae7e18a4bdc98a6fdd1e7
surajkumarsinha <suraj.sinha70@gmail.com>
2025-05-01 21:24:31 +0530

    Finite producer and consumer problem

--- ClassicProblems/src/main/java/org/example/classic/ProducerConsumer.java
+++ ClassicProblems/src/main/java/org/example/classic/ProducerConsumer.java
@@ -1,7 +1,70 @@
 package org.example.classic;
 
-public class ProducerConsumer {
-	public static void main(String[] args) {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
 
+public class ProducerConsumer {
+	private static final Semaphore items = new Semaphore(0);
+	private static final Semaphore bufferSize = new Semaphore(5);
+	private static final Semaphore mutex = new Semaphore(1);
+	private static final Queue<String> events = new LinkedList<>();
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread.ofVirtual().start(new Producer());
+		Thread.ofVirtual().start(new Consumer());
+		Thread.sleep(20000);
+	}
+
+
+	static class Producer implements Runnable {
+		int count = 0;
+		@Override
+		public void run() {
+			while(!Thread.currentThread().isInterrupted()) {
+				try {
+					String event = produceEvent(count);
+					bufferSize.acquire();
+					mutex.acquire();
+					events.add(event);
+					mutex.release();
+					items.release();
+					count++;
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+
+			}
+		}
+
+		private String produceEvent(int count) throws InterruptedException {
+			Thread.sleep(500);
+			String event = "Event-" + count;
+			System.out.println("Produced: " + event);
+			return event;
+		}
+	}
+
+	static class Consumer implements Runnable {
+		@Override
+		public void run() {
+			while(!Thread.currentThread().isInterrupted()) {
+				try {
+					items.acquire();
+					mutex.acquire();
+					String event = events.poll();
+					mutex.release();
+					bufferSize.release();
+					consumeEvent(event);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+
+		private void consumeEvent(String event) throws InterruptedException {
+			Thread.sleep(1000);
+			System.out.println("Consumed: " + event);
+		}
 	}
 }
