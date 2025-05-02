c1ebe5193661213925f8dba37aca8ac180b58d13
surajkumarsinha <suraj.sinha70@gmail.com>
2025-05-02 15:58:45 +0530

    Philosophers dining problem-update

--- ClassicProblems/src/main/java/org/example/classic/CigaretteSmokers.java
+++ ClassicProblems/src/main/java/org/example/classic/CigaretteSmokers.java
@@ -1,4 +1,120 @@
 package org.example.classic;
 
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
 public class CigaretteSmokers {
+	private final Semaphore agentSem = new Semaphore(1);
+	private final Semaphore tobacco = new Semaphore(0);
+	private final Semaphore match = new Semaphore(0);
+	private final Semaphore paper = new Semaphore(0);
+	private final List<Semaphore> sems = List.of(tobacco, match, paper);
+	// There will be 3 concurrent agent threads which will provide each ingredient
+
+	public static void main(String[] args) {
+		Table table = new Table();
+	}
+
+	final class Agent implements Runnable {
+		private Semaphore ingredientA;
+		private Semaphore ingredientB;
+		private final int id;
+
+		public Agent(int id) {
+			this.id = id;
+			for(int i=0; i<3; i++) {
+				if(id == i) continue;
+				if(ingredientA == null) ingredientA = sems.get(i);
+				else ingredientB = sems.get(i);
+			}
+		}
+
+		@Override
+		public void run() {
+			while(!Thread.currentThread().isInterrupted()) {
+				try {
+					agentSem.acquire();
+					ingredientA.release();
+					ingredientB.release();
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+
+	final class Smoker implements Runnable {
+		private final int id;
+		private Semaphore ingredient;
+		public Smoker(int id) {
+			this.id = id;
+			ingredient = sems.get(id);
+		}
+
+		@Override
+		public void run() {
+			while(!Thread.currentThread().isInterrupted()) {
+				try {
+					ingredient.acquire();
+					agentSem.release();
+					smoke();
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+
+			}
+		}
+
+		void smoke() throws InterruptedException {
+			System.out.println("Smoker-" + id + " is smoking");
+			Thread.sleep(2000);
+		}
+	}
+
+	static final class Pusher implements Runnable {
+
+
+		@Override
+		public void run() {
+			while (!Thread.currentThread().isInterrupted()) {
+
+			}
+		}
+	}
+
+	static final class Table {
+		private boolean paperAvailable;
+		private boolean tobaccoAvailable;
+		private boolean matchAvailable;
+
+		public Table() {
+			paperAvailable = false;
+			tobaccoAvailable = false;
+			matchAvailable = false;
+		}
+
+		public boolean isPaperAvailable() {
+			return paperAvailable;
+		}
+
+		public void setPaperAvailable(boolean paperAvailable) {
+			this.paperAvailable = paperAvailable;
+		}
+
+		public boolean isTobaccoAvailable() {
+			return tobaccoAvailable;
+		}
+
+		public void setTobaccoAvailable(boolean tobaccoAvailable) {
+			this.tobaccoAvailable = tobaccoAvailable;
+		}
+
+		public boolean isMatchAvailable() {
+			return matchAvailable;
+		}
+
+		public void setMatchAvailable(boolean matchAvailable) {
+			this.matchAvailable = matchAvailable;
+		}
+	}
 }
--- ClassicProblems/src/main/java/org/example/classic/PhilosophersDining.java
+++ ClassicProblems/src/main/java/org/example/classic/PhilosophersDining.java
@@ -1,4 +1,73 @@
 package org.example.classic;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 public class PhilosophersDining {
+
+	private static final Semaphore[] forks = new Semaphore[5];
+	private static final Philosopher[] philosophers = new Philosopher[5];
+	private static final Semaphore footman = new Semaphore(4);
+	public static void main(String[] args) throws InterruptedException {
+		for(int i=0; i<5; i++) {
+			forks[i] = new Semaphore(1);
+			philosophers[i] = new Philosopher(i);
+		}
+		ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+		for(int i=0; i<5; i++) executorService.submit(philosophers[i]);
+		Thread.sleep(20000);
+	}
+
+	static class Philosopher implements Runnable {
+		private final int id;
+
+		public Philosopher(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public void run() {
+			try {
+				while(!Thread.currentThread().isInterrupted()) {
+					System.out.println("Philosopher: " + id + " is hungry");
+					getForks();
+					eat();
+					putForks();
+					think();
+				}
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		private void getForks() throws InterruptedException {
+			footman.acquire();
+			getLeft(id).acquire();
+			getRight(id).acquire();
+		}
+
+		private void putForks() {
+			getLeft(id).release();
+			getRight(id).release();
+			footman.release();
+		}
+
+		private void eat() throws InterruptedException {
+			System.out.println("Philosopher: " + id + " is eating");
+			Thread.sleep(1000);
+		}
+		private void think() throws InterruptedException {
+			System.out.println("Philosopher: " + id + " is thinking");
+			Thread.sleep(2000);
+		}
+	}
+
+	static Semaphore getLeft(int i) {
+		return forks[i];
+	}
+
+	static Semaphore getRight(int i) {
+		return forks[(i+1)%5];
+	}
 }
